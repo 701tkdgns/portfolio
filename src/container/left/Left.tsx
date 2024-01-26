f44e86593a9d086f7bee345a7a75184018033253
psh <701tkdgns@inu.ac.kr>
2024-01-27 03:36:08 +0900

    update path

--- src/container/left/Left.tsx
+++ src/container/left/Left.tsx
@@ -19,7 +19,7 @@ const Left = () => {
       <div className="psh__portfolio-left-container">
         <div className="psh__portfolio-left-header">
           <div className="psh__portfolio-left-header-photo">
-            <img src="/assets/profile.jpg" alt="profile" />
+            <img src="./assets/profile.jpg" alt="profile" />
           </div>
         </div>
         <div className="psh__portfolio-left-body">
