26f092b0eb868474b1add4d2a6a08fc4b2dab245
psh <701tkdgns@inu.ac.kr>
2024-01-27 20:46:40 +0900

    add toast

--- src/container/left/Left.tsx
+++ src/container/left/Left.tsx
@@ -1,26 +1,34 @@
-import React from 'react'
-import Github from '../../component/Icons/github/Github'
-import Gmail from '../../component/popup/Gmail'
-import "./Left.css"
-import 'react-tooltip/dist/react-tooltip.css'
-import { Tooltip } from 'react-tooltip'
+import React, { useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import { Tooltip } from 'react-tooltip';
+import 'react-toastify/dist/ReactToastify.css';
+import Github from '../../component/Icons/github/Github';
+import Gmail from '../../component/popup/Gmail';
+import "./Left.css";
 
 const Left = () => {
   const homepage = process.env.PUBLIC_URL;
+
   const handleGithubLink = () => {
     window.open("https://github.com/701tkdgns", "_blank");
-  }
+  };
 
-  const clipEmail = (text: string) => {
-    navigator.clipboard.writeText(text);
-  }
+  const clipEmail = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      toast.success("Copied to clipboard");
+    } catch (error) {
+      toast.error("Failed to copy to clipboard");
+    }
+  };
 
   return (
     <div className='psh__portfolio-left'>
+      
       <div className="psh__portfolio-left-container">
         <div className="psh__portfolio-left-header">
           <div className="psh__portfolio-left-header-photo">
-            <img src={homepage+'/assets/profile.jpg'} alt="profile" />
+            <img src={homepage + '/assets/profile.jpg'} alt="profile" />
           </div>
         </div>
         <div className="psh__portfolio-left-body">
@@ -29,10 +37,10 @@ const Left = () => {
             &nbsp;
             <div className="psh__portfolio-left-body-profile-context">
               <div className='psh__portfolio-left-body-profile-list'>
-                <Github onClick={() => handleGithubLink()}/>
-                <div id="clickable">
-                  <Gmail onClick={() => clipEmail("714tkdgns@gmail.com")}/>
-                  <Tooltip anchorSelect="#clickable" clickable>
+                <Github onClick={() => handleGithubLink()} />
+                <div id="clickable" onClick={() => clipEmail("714tkdgns@gmail.com")}>
+                  <Gmail />
+                  <Tooltip id="clickable" clickable>
                     <p>714tkdgns@gmail.com</p>
                   </Tooltip>
                 </div>
@@ -41,8 +49,10 @@ const Left = () => {
           </div>
         </div>
       </div>
+      <ToastContainer />
     </div>
-  )
-}
 
-export default Left
+  );
+};
+
+export default Left;
